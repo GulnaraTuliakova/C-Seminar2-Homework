0b308c8d0150d372a0b9d8276aed8f504bbe1bc1
Gulnara Tuliakova <gulnaratulyakova@gmail.com>
2022-05-15 17:09:00 +0500

    Add HW2 Task03

---
+++ HW2 Task02/Program.cs
@@ -0,0 +1,12 @@
+﻿//По двум заданным числам проверять является ли одно квадратом другого
+Console.WriteLine("По двум числам проверим, является ли одно квадратом другого.");
+Console.WriteLine("Введите первое число:");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число:");
+int b = int.Parse(Console.ReadLine());
+if (a==b*b)
+Console.WriteLine($"Число {a} является квадратoм числа {b}");
+else if (b==a*a)
+Console.WriteLine($"Число {b} является квадратoм числа {a}");
+else
+Console.WriteLine($"Числа {a} и {b} не являются квадратом друг друга"); 
